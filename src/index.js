273d762c0f392002428d0a3e904854d5e3812d30
sucheasy <admin@sucheasy.com>
2024-09-18 21:08:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.sucheasy.com": dockerHub,
+  "quay.sucheasy.com": "https://quay.io",
+  "gcr.sucheasy.com": "https://gcr.io",
+  "k8s-gcr.sucheasy.com": "https://k8s.gcr.io",
+  "k8s.sucheasy.com": "https://registry.k8s.io",
+  "ghcr.sucheasy.com": "https://ghcr.io",
+  "cloudsmith.sucheasy.com": "https://docker.cloudsmith.io",
+  "ecr.sucheasy.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.sucheasy.com": dockerHub,
 };
 
 function routeByHosts(host) {
